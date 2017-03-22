089542a62c7c8366f9e55b6c0e676144e1b9cf3e
Pete Corey <petecorey@gmail.com>
2017-03-22 14:12:57 -0400

    Added create-react-app

--- lib/inject_detect/endpoint.ex
+++ lib/inject_detect/endpoint.ex
@@ -3,13 +3,19 @@ defmodule InjectDetect.Endpoint do
 
   socket "/socket", InjectDetect.UserSocket
 
+  plug Plug.Static.IndexHtml,
+    at: "/"
+
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
   plug Plug.Static,
-    at: "/", from: :inject_detect, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    at: "/", 
+    from: "priv/inject-detect/build/",
+    gzip: false,
+    only: ~w(index.html favicon.ico static)
+    # only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
