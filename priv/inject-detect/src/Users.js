17d6898c765940d47b15ad4d6581795a1b89a1a6
Pete Corey <petecorey@gmail.com>
2017-03-22 16:18:16 -0400

    Instead of polling

--- priv/inject-detect/src/Users.js
+++ priv/inject-detect/src/Users.js
@@ -2,9 +2,9 @@ import React from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 
-function GetStarted({mutate, userId}) {
+function GetStarted({mutate, userId, onGetStarted}) {
     return (
-        <button onClick={() => mutate({ variables: { email: `foo+${Math.random()}@email.com` }})}>
+            <button onClick={() => mutate({ variables: { email: `foo+${Math.random()}@email.com` }}) && onGetStarted()}>
             Get started
         </button>
     )
@@ -20,17 +20,17 @@ const GetStartedWrapped = graphql(gql`
 
 // The data prop, which is provided by the wrapper below contains,
 // a `loading` key while the query is in flight and posts when it is ready
-function Users({ data: { loading, users } }) {
+function Users({ data: { loading, users, refetch } }) {
     if (loading) {
         return <div>Loading</div>;
     } else {
-        console.log(users)
+        let onGetStarted = () => refetch();
         return (
             <div>
                 <ul>
                     {users.map(user => <li key={user.id}>{user.email}</li>)}
                 </ul>
-                <GetStartedWrapped/>
+                <GetStartedWrapped onGetStarted={onGetStarted}/>
             </div>
         );
     }
@@ -45,4 +45,6 @@ export default graphql(gql`
             email
         }
     }
-`)(Users);
+`, {
+    // options: { pollInterval: 1000 },
+})(Users);
