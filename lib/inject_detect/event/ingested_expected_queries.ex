d4aafb1d0751520047658c491db8e4ca1ded8050
Pete Corey <petecorey@gmail.com>
2017-04-05 16:27:48 -0400

    Better ingested_expected_queries

--- lib/inject_detect/event/ingested_expected_queries.ex
+++ lib/inject_detect/event/ingested_expected_queries.ex
@@ -9,18 +9,62 @@ end
 defimpl InjectDetect.State.Reducer,
    for: InjectDetect.Event.IngestedExpectedQueries do
 
+  # defp that_match(collection, query, type) do
+  #   fn (:get_and_update, queries, next) ->
+  #     queries
+  #     |> Enum.map(fn
+  #       expected = %{collection: ^collection,
+  #                     query: ^query,
+  #                     type: ^type}  -> {true, next.(expected)}
+  #       expected                  -> {false, {expected, expected}}
+  #     end)
+  #     |> (fn
+  #           queries -> found = queries
+  #                     |> Enum.map(&(elem(&1, 0)))
+  #                     |> Enum.any?
+  #                     case found do
+  #                       true  -> Enum.map(queries, &(elem(&1, 1)))
+  #                       false -> Enum.map(queries, &(elem(&1, 1))) ++ [next.(nil)]
+  #                     end
+  #         end).()
+  #     |> :lists.unzip
+  #   end
+  # end
+
   def apply(event, state) do
+    # TODO: Subtract credits
     state
-    # application = Map.from_struct(event)
-    # |> Map.delete(:application_id)
-    # |> Map.put_new(:id, event.application_id)
-    # state = update_in(state,
-    #   [:users,
-    #    InjectDetect.State.all_with(id: event.user_id),
-    #    :applications],
-    #   fn applications ->
-    #     applications ++ [application]
-    #   end)
+
+    # event.queries
+    # |> Enum.reduce(state, fn
+    #   (%{"collection" => collection,
+    #      "query" => query,
+    #      "queried_at" => queried_at,
+    #      "type" => type}, state) ->
+    #     update_in(state,
+    #       [:users,
+    #        Access.all,
+    #        :applications,
+    #        InjectDetect.State.all_with(id: event.application_id),
+    #        :expected_queries,
+    #        that_match(collection, query, type)], fn
+
+    #         # New expected query
+    #         nil ->
+    #           %{collection: collection,
+    #             count: 1,
+    #             query: query,
+    #             type: type,
+    #             last_queried_at: queried_at}
+
+    #         # Previous seen expected query
+    #         expected ->
+    #           %{expected |
+    #             count: expected.count + 1,
+    #             last_queried_at: queried_at}
+
+    #       end)
+    # end)
   end
 
 end
