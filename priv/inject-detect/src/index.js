a3c1fc862226103aec0f720107e4efdfad2de4e8
Pete Corey <petecorey@gmail.com>
2017-05-16 15:04:49 -0400

    Added scrollToTop

--- priv/inject-detect/src/index.js
+++ priv/inject-detect/src/index.js
@@ -7,6 +7,7 @@ import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import React from "react";
 import ReactDOM from "react-dom";
+import ScrollToTop from "./components/ScrollToTop";
 import SignIn from "./components/SignIn";
 import UnexpectedQuery from "./components/UnexpectedQuery";
 import VerifyRequestedToken from "./components/VerifyRequestedToken";
@@ -18,19 +19,21 @@ import { client } from "./apollo";
 ReactDOM.render(
     (
         <ApolloProvider client={client}>
-            <BrowserRouter>
-                <div className="ij-layout">
-                    <Header/>
-                    <div className="ij-layout-content ui main container">
-                        <Route path="/sign-in" component={SignIn}/>
-                        <Route path="/get-started" component={GetStarted}/>
-                        <Route path="/verify/:token" component={VerifyRequestedToken}/>
-                        <PrivateRoute exact path="/" component={Dashboard}/>
-                        <PrivateRoute path="/account" component={Account}/>
-                        <PrivateRoute path="/application/:id" component={Application}/>
-                        <PrivateRoute path="/unexpected-query/:id" component={UnexpectedQuery}/>
+            <BrowserRouter onUpdate={() => window.scrollTo(0, 0)}>
+                <ScrollToTop>
+                    <div className="ij-layout">
+                        <Header/>
+                        <div className="ij-layout-content ui main container">
+                            <Route path="/sign-in" component={SignIn}/>
+                            <Route path="/get-started" component={GetStarted}/>
+                            <Route path="/verify/:token" component={VerifyRequestedToken}/>
+                            <PrivateRoute exact path="/" component={Dashboard}/>
+                            <PrivateRoute path="/account" component={Account}/>
+                            <PrivateRoute path="/application/:id" component={Application}/>
+                            <PrivateRoute path="/unexpected-query/:id" component={UnexpectedQuery}/>
+                        </div>
                     </div>
-                </div>
+                </ScrollToTop>
             </BrowserRouter>
         </ApolloProvider>
     ),
