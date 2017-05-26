93f81d683365a364e62a47a1590125df52e16807
Pete Corey <petecorey@gmail.com>
2017-05-26 14:33:26 -0400

    stackable menu

--- priv/inject-detect/src/components/Header.js
+++ priv/inject-detect/src/components/Header.js
@@ -1,17 +1,16 @@
-import AddApplicationModal from "./AddApplicationModal";
-import React from "react";
-import SignOutLink from "./SignOutLink";
-import _ from "lodash";
-import { Link, NavLink } from "react-router-dom";
-import { Redirect } from "react-router-dom";
-import { UserQuery } from "../graphql";
-import { graphql } from "react-apollo";
+import AddApplicationModal from './AddApplicationModal';
+import React from 'react';
+import SignOutLink from './SignOutLink';
+import _ from 'lodash';
+import { Link, NavLink } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
+import { UserQuery } from '../graphql';
+import { graphql } from 'react-apollo';
 
 class Header extends React.Component {
-
     state = {
         redirect: false
-    }
+    };
 
     initDropdown() {
         window.$('.ui.dropdown').dropdown();
@@ -27,63 +26,67 @@ class Header extends React.Component {
 
     showAddApplicationModal(e) {
         e.preventDefault();
-        window.$(".ui.add-application-modal.modal").modal("show");
+        window.$('.ui.add-application-modal.modal').modal('show');
     }
 
     onSignOut = () => {
         this.setState({ redirect: true });
-    }
+    };
 
     render() {
         let { user } = this.props.data;
 
         if (user) {
             return (
-                <div className="ui menu ij-header">
-                    {
-                        this.state.redirect ? (
-                            <Redirect to="/"/>
-                        ) : (
-                            null
-                        )
-                    }
+                <div className="ui stackable menu ij-header">
+                    {this.state.redirect ? <Redirect to="/" /> : null}
                     <div className="ui container">
                         <Link to="/" className="header borderless item">
-                            <img alt="Inject Detect Logo" src="https://s3.amazonaws.com/www.injectdetect.com/logo.png" className="icon"/>
+                            <img
+                                alt="Inject Detect Logo"
+                                src="https://s3.amazonaws.com/www.injectdetect.com/logo.png"
+                                className="icon"
+                            />
                             Inject Detect
                         </Link>
 
                         <NavLink to="/" exact={true} className="borderless item">Dashboard</NavLink>
 
                         <div className="ui simple dropdown borderless item">
-                            Applications <i className="dropdown icon"></i>
+                            Applications <i className="dropdown icon" />
                             <div className="menu">
 
-                                { user &&
-                                  user.applications &&
-                                  user.applications.map((application) => {
-                                      return (
-                                          <Link to={`/application/${application.id}`} key={application.id} className="item"><i className="server outline icon"/>{application.name}</Link>
-                                      );
-                                  })}
+                                {user &&
+                                    user.applications &&
+                                    user.applications.map(application => {
+                                        return (
+                                            <Link
+                                                to={`/application/${application.id}`}
+                                                key={application.id}
+                                                className="item"
+                                            >
+                                                <i className="server outline icon" />{application.name}
+                                            </Link>
+                                        );
+                                    })}
 
-                                <AddApplicationModal user={user}/>
+                                <AddApplicationModal user={user} />
                             </div>
                         </div>
 
                         <div className="right menu">
                             <div className="borderless item">
                                 <button id="userbutton" className="ui right labeled icon brand dropdown button">
-                                    <i className="caret down icon"></i>
+                                    <i className="caret down icon" />
                                     {user.email}
                                     <div className="menu">
                                         <Link to="/account" className="item">
-                                            <i className="settings icon"></i>
+                                            <i className="settings icon" />
                                             Account settings
                                         </Link>
-                                        <div className="divider"></div>
+                                        <div className="divider" />
                                         <SignOutLink onSignOut={this.onSignOut}>
-                                            <i className="sign out icon"></i>
+                                            <i className="sign out icon" />
                                             Sign out
                                         </SignOutLink>
                                     </div>
@@ -93,20 +96,17 @@ class Header extends React.Component {
                     </div>
                 </div>
             );
-        }
-        else {
+        } else {
             return (
                 <div className="ui menu ij-header">
-                    {
-                        this.state.redirect ? (
-                            <Redirect to="/"/>
-                        ) : (
-                            null
-                        )
-                    }
+                    {this.state.redirect ? <Redirect to="/" /> : null}
                     <div className="ui container">
                         <Link to="/" className="header borderless item">
-                            <img alt="Inject Detect Logo" src="https://s3.amazonaws.com/www.injectdetect.com/logo.png" className="icon"/>
+                            <img
+                                alt="Inject Detect Logo"
+                                src="https://s3.amazonaws.com/www.injectdetect.com/logo.png"
+                                className="icon"
+                            />
                             Inject Detect
                         </Link>
 
@@ -129,15 +129,15 @@ export default graphql(UserQuery, {
         return {
             reducer: (previousResults, action) => {
                 switch (action.operationName) {
-                    case "verifyRequestedToken":
+                    case 'verifyRequestedToken':
                         return _.extend({}, previousResults, {
-                            user: _.get(action, "result.data.verifyRequestedToken")
+                            user: _.get(action, 'result.data.verifyRequestedToken')
                         });
-                    case "getStarted":
+                    case 'getStarted':
                         return _.extend({}, previousResults, {
-                            user: _.get(action, "result.data.getStarted")
+                            user: _.get(action, 'result.data.getStarted')
                         });
-                    case "signOut":
+                    case 'signOut':
                         return _.extend({}, previousResults, {
                             user: null
                         });
