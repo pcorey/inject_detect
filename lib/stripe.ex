01659643b2d75098cd44cdaf707b680d8c170885
Pete Corey <petecorey@gmail.com>
2017-06-01 17:09:40 -0400

    Stripe refactor

--- lib/stripe.ex
+++ lib/stripe.ex
@@ -2,25 +2,20 @@ defmodule Stripe do
 
   @base_url "https://api.stripe.com/v1"
 
-  def create_customer(user_id, source) do
-    stripe_secret = System.get_env("STRIPE_SECRET")
-    body = {:form, [description: user_id, source: source]}
-    headers = [{"Authorization", "Bearer #{stripe_secret}"}, {"Content-Type", "application/json"}]
-    with {:ok, response } <- HTTPoison.post("#{@base_url}/customers", body, headers) do
+  defp post(route, body) do
+    headers = [{"Authorization", "Bearer #{System.get_env("STRIPE_SECRET)}"},
+               {"Content-Type", "application/json"}]
+    with {:ok, response } <- HTTPoison.post("#{@base_url}/#{route}", {:form, body}, headers) do
       Poison.decode(response.body)
     end
   end
 
+  def create_customer(user_id, source) do
+    post("customers", [description: user_id, source: source])
+  end
+
   def charge_customer(customer_id, credits, amount) do
-    stripe_secret = System.get_env("STRIPE_SECRET")
-    body = {:form, [amount: amount,
-                    currency: "usd",
-                    description: "Charge for #{credits} credits",
-                    customer: customer_id]}
-    headers = [{"Authorization", "Bearer #{stripe_secret}"}, {"Content-Type", "application/json"}]
-    with {:ok, response } <- HTTPoison.post("#{@base_url}/charges", body, headers) do
-      Poison.decode(response.body)
-    end
+    post("charges", [amount: amount, currency: "usd", customer: customer_id])
   end
 
   def get_charges(customer_id) do
