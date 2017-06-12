0fb4d54098bda0a27054189796cfaec2367f1033
Pete Corey <petecorey@gmail.com>
2017-06-12 14:32:56 -0400

    Added welcome dashboard page

--- priv/inject-detect/src/components/Dashboard.js
+++ priv/inject-detect/src/components/Dashboard.js
@@ -26,12 +26,81 @@ class Dashboard extends React.Component {
             return <div className="ui active loader" />;
         }
 
+        let expectedQueries = _.get(user, 'applications.expectedQueries');
+
         let unexpectedQueries = _.chain(user.applications)
             .map('unexpectedQueries')
             .flatten()
             .sortBy('queriedAt')
             .reverse()
             .value();
+
+        if (_.isEmpty(expectedQueries) && _.isEmpty(unexpectedQueries)) {
+            return (
+                <div className="ij-dashboard ui mobile stackable grid">
+                    <div className="sixteen wide column">
+                        <h1 className="ui header">
+                            Welcome to Inject Detect!
+                        </h1>
+                    </div>
+                    <div className="section" style={{ marginTop: 0 }}>
+                        {/* <h3 className="ui sub header">Credits:</h3> */}
+                        <p className="instructions">
+                            We've gone ahead and set up your
+                            {' '}
+                            <Link to={`/application/${user.applications[0].id}`}>first application</Link>
+                            . Check out our
+                            {' '}
+                            <a href="#">Getting Started</a>
+                            {' '}
+                            guide to get your Meteor application up and running with your new Inject Detect account.
+                            {' '}
+                        </p>
+                        <hr
+                            style={{ border: 0, 'border-bottom': '1px solid #ddd', width: '75%', margin: '2em auto' }}
+                        />
+                        <p className="instructions">
+                            Your application is currently in
+                            {' '}
+                            <strong>training mode</strong>
+                            . Every query made by your application will automatically be marked as an expected query. While in training mode, run your application through all of it's normal user stories to generate your application's complete set of expected queries. Once you're satisfied that you've captured every query made by your application,
+                            {' '}
+                            <strong>turn off training mode</strong>
+                            .
+                        </p>
+                        <p className="instructions">
+                            Once out of training mode, we'll compare every query made by your application against your set of expected queries. If we detect an
+                            {' '}
+                            <strong>unexpected query</strong>
+                            , we'll send you en email alert, letting you know immediately.
+                        </p>
+                        <hr
+                            style={{ border: 0, 'border-bottom': '1px solid #ddd', width: '75%', margin: '2em auto' }}
+                        />
+                        <p className="instructions">
+                            Every query your application makes consumes an Inject Detect
+                            {' '}
+                            <strong>credit</strong>
+                            . Your account is initially loaded with 10,000 credits, which means we'll handle up to 10,000 queries made by your application. Once your account runs out of credits, we'll stop monitoring incoming queries made by your application.
+                        </p>
+                        <p className="instructions">
+                            You can
+                            {' '}
+                            <strong>buy more credits</strong>
+                            {' '}
+                            or configure
+                            {' '}
+                            <strong>automatic token purchases </strong>
+                            in your
+                            {' '}
+                            <Link to="/account">account settings</Link>
+                            .
+                        </p>
+                    </div>
+
+                </div>
+            );
+        }
 
         return (
             <div className="ij-dashboard ui mobile stackable grid">
