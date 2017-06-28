6553d0e7084db8e662ca52fc0945c7be35a4247d
Pete Corey <petecorey@gmail.com>
2017-06-28 14:58:25 -0400

    Added turn off/on alerting

---
+++ lib/inject_detect/command/turn_off_alerting.ex
@@ -0,0 +1,25 @@
+defmodule InjectDetect.Command.TurnOffAlerting do
+  defstruct user_id: nil
+end
+
+defimpl InjectDetect.Command, for: InjectDetect.Command.TurnOffAlerting do
+
+  alias InjectDetect.Event.TurnedOffAlerting
+  alias InjectDetect.State.User
+
+  def toggle_alerting(user = %{id: user_id}, command, %{user_id: user_id}) do
+    {:ok, [%TurnedOffAlerting{user_id: user_id}]}
+  end
+
+  def toggle_alerting(_, _, _) do
+    {:error, %{code: :not_authorized,
+               error: "Not authorized",
+               message: "Not authorized"}}
+  end
+
+  def handle(command, context) do
+    User.find(command.user_id)
+    |> toggle_alerting(command, context)
+  end
+
+end
---
+++ lib/inject_detect/command/turn_on_alerting.ex
@@ -0,0 +1,25 @@
+defmodule InjectDetect.Command.TurnOnAlerting do
+  defstruct user_id: nil
+end
+
+defimpl InjectDetect.Command, for: InjectDetect.Command.TurnOnAlerting do
+
+  alias InjectDetect.Event.TurnedOnAlerting
+  alias InjectDetect.State.User
+
+  def toggle_alerting(user = %{id: user_id}, command, %{user_id: user_id}) do
+    {:ok, [%TurnedOnAlerting{user_id: user_id}]}
+  end
+
+  def toggle_alerting(_, _, _) do
+    {:error, %{code: :not_authorized,
+               error: "Not authorized",
+               message: "Not authorized"}}
+  end
+
+  def handle(command, context) do
+    User.find(command.user_id)
+    |> toggle_alerting(command, context)
+  end
+
+end
