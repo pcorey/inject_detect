4d3054c120133c422d221afebe0ae00f91578f04
Pete Corey <petecorey@gmail.com>
2017-07-11 08:22:44 -0400

    Added todo

--- lib/inject_detect/event/ingested_query.ex
+++ lib/inject_detect/event/ingested_query.ex
@@ -11,6 +11,7 @@ defimpl InjectDetect.State.Reducer,
    for: InjectDetect.Event.IngestedQuery do
 
   def apply(event, state) do
+    # TODO: Add ingest to hourly buckets. 24 hours * 31 days = 744 buckets per user.
     update_in(state, [Lens.key(:users),
                       Lens.filter(&(&1.id == event.user_id)),
                       Lens.key(:ingests_pending_invoice)], &(&1 + 1))
