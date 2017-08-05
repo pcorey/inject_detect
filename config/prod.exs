f7f57b35439ad82b186bbe0f555ca6a74f14335d
Pete Corey <petecorey@gmail.com>
2017-08-05 18:23:02 -0400

    Updated prod config

--- config/prod.exs
+++ config/prod.exs
@@ -28,7 +28,7 @@ config :inject_detect, InjectDetect.Endpoint,
 # Configure your database
 config :inject_detect, InjectDetect.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
+  url: {:system, "DATABASE_URL"},
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
 
