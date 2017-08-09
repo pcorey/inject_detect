ecb64eea06bb289233508d9663e0d6435da9958f
Pete Corey <petecorey@gmail.com>
2017-08-09 17:42:29 -0400

    Front-end build

--- priv/inject-detect/src/components/Account.js
+++ priv/inject-detect/src/components/Account.js
@@ -56,13 +56,16 @@ class Account extends React.Component {
                                     </p>
                                     <AccountSubscription />
                                 </div>
-                              : <p className="instructions">
-                                    You haven't added a payment method to your account. Be sure to add a payment method before your trail runs out to avoid interruptions in service. Read about
-                                    {' '}
-                                    <a href="http://www.injectdetect.com/pricing/">our pricing and billing</a>
-                                    {' '}
-                                    for more information.
-                                </p>
+                              : <div>
+                                    <p className="instructions">
+                                        You haven't added a payment method to your account. Be sure to add a payment method before your trail runs out to avoid interruptions in service. Read about
+                                        {' '}
+                                        <a href="http://www.injectdetect.com/pricing/">our pricing and billing</a>
+                                        {' '}
+                                        for more information.
+                                    </p>
+                                    <AccountSubscription />
+                                </div>
                         : <div>
                               <p className="instructions">
                                   Your account is decativated. Inject Detect is no longer monitoring your applications for NoSQL Injection attacks. To re-activate your account,
